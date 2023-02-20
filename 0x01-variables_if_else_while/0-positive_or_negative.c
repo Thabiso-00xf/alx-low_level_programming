78d21e1933e35ff7e48bfb7950ec3cac983f6ddc
Thabiso-00xf <thabisosindane666@gmail.com>
2023-02-20 11:16:13 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
- *
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
- srand(time(0));
- n = rand() - RAND_MAX / 2;
-/* your code goes there */ 
- if (n > 0)
-{
- printf("%d is positive\n", n);
- }
- if (n < 0)
- {
- printf("%d is negative\n", n);
- }
- if (n == 0)
- {
- printf("%d is zero\n", n);
- }
- return (0);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
+

174dac5ef5cb34e49e5d9f745c7365db67c62ba1
Thabiso-00xf <thabisosindane666@gmail.com>
2023-03-22 12:29:16 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "func_pointers.h"
+
+/**
+ * print_name - name of function
+ * @name: pointer
+ * @f: funcrion pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}

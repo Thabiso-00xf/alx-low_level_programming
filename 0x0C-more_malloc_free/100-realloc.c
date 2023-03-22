9c522ed0206bb11d060e70c9f488346505606a4b
Thabiso-00xf <thabisosindane666@gmail.com>
2023-03-22 13:36:48 -0700

    new addition

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - fuction name
+ * @ptr: value one
+ * @old_size: value two
+ * @new_size: value three
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int a;
+}

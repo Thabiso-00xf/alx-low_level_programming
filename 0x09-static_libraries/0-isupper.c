220679e91db7f5b832cc052ee83fcb976894d332
Thabiso-00xf <thabisosindane666@gmail.com>
2023-08-04 04:47:45 -0700

    islower

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -7,7 +7,7 @@
  *
  */
 
-int _putchar(char b)
+int _putchar(char c)
 {
-	return (write(1, &b, 1))
+	return (write(1, &c, 1))
 }		
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** a prototype that detect that whether
+ * the alphabet is a lowercase, it validate the input
+ * if its true it return 1 else return zero
+ *
+ */
+
+int _islower(int c)
+
+{
+if (c >= 97 && c <= 122) /*number reference from ascii table*/
+	return (1);
+{
+	return (0);
+}
+}
